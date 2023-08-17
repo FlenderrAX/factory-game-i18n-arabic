a147600a012452c1d5ed938f13f16fc5a74c7829
Elanis <elanis@hotmail.com>
2023-08-17 19:19:04 +0200

    Add credits & decisions

--- resources.js
+++ resources.js
@@ -3,12 +3,16 @@
 import achievements_en from './achievements/en.json';
 import buildings_en from './buildings/en.json';
 import contracts_en from './contracts/en.json';
+import credits_en from './credits/en.json';
+import durations_en from './durations/en.json';
 import features_en from './features/en.json';
 import resources_en from './resources/en.json';
 
 import achievements_fr from './achievements/fr.json';
 import buildings_fr from './buildings/fr.json';
 import contracts_fr from './contracts/fr.json';
+import credits_fr from './credits/fr.json';
+import durations_fr from './durations/fr.json';
 import features_fr from './features/fr.json';
 import resources_fr from './resources/fr.json';
 
@@ -17,6 +21,8 @@ resources['en'] = {
 	achievements: achievements_en,
 	buildings: buildings_en,
 	contracts: contracts_en,
+	credits: credits_en,
+	durations: durations_en,
 	features: features_en,
 	resources: resources_en,
 };
@@ -24,6 +30,8 @@ resources['fr'] = {
 	achievements: achievements_fr,
 	buildings: buildings_fr,
 	contracts: contracts_fr,
+	credits: credits_fr,
+	durations: durations_fr,
 	features: features_fr,
 	resources: resources_fr,
 };
