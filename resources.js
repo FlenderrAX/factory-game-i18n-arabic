2941439dfdf48cd470079351285c91f8ac08effe
Elanis <elanis@hotmail.com>
2023-11-06 17:47:10 +0100

    Big refacto
    - Rename "building" to "equipment"
    - Remove tons of wrong plural
    - Make it more future-proof

--- resources.js
+++ resources.js
@@ -1,7 +1,7 @@
 // This file is needed because vite doesn't like dynamic import when exporting tauri ...
 
 import achievements_en from './achievements/en.json';
-import buildings_en from './buildings/en.json';
+import equipments_en from './equipments/en.json';
 import contracts_en from './contracts/en.json';
 import credits_en from './credits/en.json';
 import durations_en from './durations/en.json';
@@ -9,7 +9,7 @@ import features_en from './features/en.json';
 import resources_en from './resources/en.json';
 
 import achievements_fr from './achievements/fr.json';
-import buildings_fr from './buildings/fr.json';
+import equipments_fr from './equipments/fr.json';
 import contracts_fr from './contracts/fr.json';
 import credits_fr from './credits/fr.json';
 import durations_fr from './durations/fr.json';
@@ -19,7 +19,7 @@ import resources_fr from './resources/fr.json';
 const resources = {};
 resources['en'] = {
 	achievements: achievements_en,
-	buildings: buildings_en,
+	equipments: equipments_en,
 	contracts: contracts_en,
 	credits: credits_en,
 	durations: durations_en,
@@ -28,7 +28,7 @@ resources['en'] = {
 };
 resources['fr'] = {
 	achievements: achievements_fr,
-	buildings: buildings_fr,
+	equipments: equipments_fr,
 	contracts: contracts_fr,
 	credits: credits_fr,
 	durations: durations_fr,
