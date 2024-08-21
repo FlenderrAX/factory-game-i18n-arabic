51506033470f1a82e1d99d6b382769fbc8b2349a
Elanis <elanis@hotmail.com>
2024-08-21 14:10:26 +0200

    #1709 - Feedback

--- resources.js
+++ resources.js
@@ -6,6 +6,7 @@ import contracts_en from './contracts/en.json';
 import credits_en from './credits/en.json';
 import durations_en from './durations/en.json';
 import features_en from './features/en.json';
+import feedback_en from './feedback/en.json';
 import resources_en from './resources/en.json';
 
 import achievements_fr from './achievements/fr.json';
@@ -14,6 +15,7 @@ import contracts_fr from './contracts/fr.json';
 import credits_fr from './credits/fr.json';
 import durations_fr from './durations/fr.json';
 import features_fr from './features/fr.json';
+import feedback_fr from './feedback/fr.json';
 import resources_fr from './resources/fr.json';
 
 const resources = {};
@@ -24,6 +26,7 @@ resources['en'] = {
 	credits: credits_en,
 	durations: durations_en,
 	features: features_en,
+	feedback: feedback_en,
 	resources: resources_en,
 };
 resources['fr'] = {
@@ -33,6 +36,7 @@ resources['fr'] = {
 	credits: credits_fr,
 	durations: durations_fr,
 	features: features_fr,
+	feedback: feedback_fr,
 	resources: resources_fr,
 };
 
