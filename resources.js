d489a029fb1be61817ed10c85b6397a2b8a2e805
Elanis <elanis@hotmail.com>
2024-08-22 11:03:02 +0200

    Menu i18n

--- resources.js
+++ resources.js
@@ -7,6 +7,7 @@ import credits_en from './credits/en.json';
 import durations_en from './durations/en.json';
 import features_en from './features/en.json';
 import feedback_en from './feedback/en.json';
+import menu_en from './menu/en.json';
 import resources_en from './resources/en.json';
 
 import achievements_fr from './achievements/fr.json';
@@ -16,6 +17,7 @@ import credits_fr from './credits/fr.json';
 import durations_fr from './durations/fr.json';
 import features_fr from './features/fr.json';
 import feedback_fr from './feedback/fr.json';
+import menu_fr from './menu/fr.json';
 import resources_fr from './resources/fr.json';
 
 const resources = {};
@@ -27,6 +29,7 @@ resources['en'] = {
 	durations: durations_en,
 	features: features_en,
 	feedback: feedback_en,
+	menu: menu_en,
 	resources: resources_en,
 };
 resources['fr'] = {
@@ -37,6 +40,7 @@ resources['fr'] = {
 	durations: durations_fr,
 	features: features_fr,
 	feedback: feedback_fr,
+	menu: menu_fr,
 	resources: resources_fr,
 };
 
